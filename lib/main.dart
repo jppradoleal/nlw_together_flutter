ea79ba5a15299b812b71dcad1e17b6e4c55d6846
Joao Pedro Prado Leal <joaopedro0128@hotmail.com>
2021-06-26 22:39:41 -0300

    feat: done login and splash page

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:nlw_together/modules/home/home_page.dart';
+import 'package:nlw_together/modules/login/login_page.dart';
+import 'package:nlw_together/modules/splash/splash_page.dart';
+import 'package:nlw_together/shared/themes/app_colors.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(AppWidget());
 }
 
-class MyApp extends StatelessWidget {
+class AppWidget extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.purple,
+        primaryColor: AppColors.primary,
       ),
-      home: MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key key}) : super(key: key);
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int count = 0;
-
-  void increment() {
-    setState(() {
-      count++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("App")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          increment();
-        },
-        child: Icon(Icons.add),
-      ),
-      body: Center(
-        child: Text("Contador: $count"),
-      ),
+      home: HomePage(),
     );
   }
 }
---
+++ lib/modules/login/login_page.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:nlw_together/shared/themes/app_colors.dart';
+import 'package:nlw_together/shared/themes/app_images.dart';
+import 'package:nlw_together/shared/themes/app_text_styles.dart';
+import 'package:nlw_together/shared/widgets/social_login/social_login_button.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Container(
+        width: size.width,
+        height: size.height,
+        child: Stack(
+          children: [
+            Container(
+              color: AppColors.primary,
+              width: size.width,
+              height: size.height * 0.36,
+            ),
+            Positioned(
+              child: Image.asset(
+                AppImages.person,
+                width: 208,
+                height: 300,
+              ),
+              top: 80,
+              right: 0,
+              left: 0,
+            ),
+            Positioned(
+              right: 0,
+              bottom: size.height * 0.15,
+              left: 0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.asset(AppImages.logomini),
+                  Padding(
+                    padding: EdgeInsets.only(top: 30, left: 70, right: 70),
+                    child: Text(
+                      "Organize seus boletos em um só lugar",
+                      textAlign: TextAlign.center,
+                      style: TextStyles.titleHome,
+                    ),
+                  ),
+                  Padding(
+                    padding:
+                        const EdgeInsets.only(left: 40, right: 40, top: 40),
+                    child: SocialLoginButton(
+                      onTap: () {
+                        print("Clicou");
+                      },
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
